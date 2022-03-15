aec7520dcc018a62b47481bb4458c8d6ff4b87f6
Aleksandr Nazarov <nazarov.alexander2013@gmail.com>
2022-03-16 01:32:19 +0300

    Added Bottom Navigation Menu. UI complete

---
+++ app/src/main/java/com/mytestprogram/jetpackcomposetraining/BottomMenuContent.kt
@@ -0,0 +1,8 @@
+package com.mytestprogram.jetpackcomposetraining
+
+import androidx.annotation.DrawableRes
+
+data class BottomMenuContent(
+    val title: String,
+    @DrawableRes val iconId: Int
+)
--- app/src/main/java/com/mytestprogram/jetpackcomposetraining/MainActivity.kt
+++ app/src/main/java/com/mytestprogram/jetpackcomposetraining/MainActivity.kt
@@ -3,11 +3,18 @@ package com.mytestprogram.jetpackcomposetraining
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.ExperimentalFoundationApi
+import com.mytestprogram.jetpackcomposetraining.ui.HomeScreen
+import com.mytestprogram.jetpackcomposetraining.ui.theme.JetpackComposeTrainingTheme
 
+@ExperimentalFoundationApi
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            JetpackComposeTrainingTheme {
+                HomeScreen()
+            }
         }
     }
 }
